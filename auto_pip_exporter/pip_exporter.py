742a70551450ed2ccb9f4c281b1dfb39660495fa
hdirks2 <114555560+hdirks2@users.noreply.github.com>
2023-04-10 00:47:45 -0700

    All done with the code! Just gotta do documentatio

--- auto_pip_exporter/pip_exporter.py
+++ auto_pip_exporter/pip_exporter.py
@@ -1,22 +1,23 @@
 # Imported Libraries
 from dotenv import load_dotenv
+from pyautogui import write, press
 
 # Python Libraries
-from os import getenv
+from os import getenv, system
 from os.path import dirname, join, isfile
 from sys import argv
-from time import sleep
 
 # Constants
 ENV_NAME = "pip_account_info.env"  # CHANGE THIS TO YOUR ENVIRONMENT NAME (.env file)
 ENV_PATH = join(dirname(__file__), ENV_NAME)
 
-# Main Function
-def pip_exporter(**kwargs) -> bool:
+
+# Export to PyPi given a version
+def pypi_exporter(**kwargs) -> bool:
     if not isfile(ENV_PATH):    # If the environment file is not found, create it & return False
         print(f"Environment file not found: {ENV_PATH}\nPlease input your pip account information in the newly made .env file")
         with open(ENV_PATH, "w") as f:
-            f.write("# Pip Account Details\nUSERNAME = \nPASSWORD = ")
+            f.write("# Pip Account Details\nUSERNAME = \"\"\nPASSWORD = \"\"")
         return False
     if not kwargs:  # If the version is not provided, return False
         if len(argv) == 2:
@@ -24,11 +25,6 @@ def pip_exporter(**kwargs) -> bool:
         else:
             print("Please provide a package version")
             return False
-
-    # Get Environment Variables   
-    load_dotenv(ENV_PATH)
-    USERNAME = getenv("USERNAME")
-    PASSWORD = getenv("PASSWORD")
 
     # Get Version for pip package
     if argv[1]:
@@ -53,8 +49,16 @@ def pip_exporter(**kwargs) -> bool:
     # Write the new lines to setup.py
     with open("setup.py", "w") as f:
         f.writelines(lines)
+
+    # Run setup.py then begin uploading to PyPi
+    system("python3 setup.py sdist bdist_wheel")
+
+    # Get PyPi Account Variables   
+    load_dotenv(ENV_PATH)
+    USERNAME = getenv("USERNAME")
+    PASSWORD = getenv("PASSWORD")
+    system(f"twine upload dist/* -u \"{USERNAME}\" -p \"{PASSWORD}\"")
     
 
-
 if __name__ == "__main__":
-    pip_exporter()
+    pypi_exporter()
