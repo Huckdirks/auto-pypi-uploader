c7378a92960b63e94bdf4fcf6116b8e1654df1ad
hdirks2 <114555560+hdirks2@users.noreply.github.com>
2023-04-27 22:02:12 -0700

    Changed .env file name

--- auto_pypi_uploader/pypi_uploader.py
+++ auto_pypi_uploader/pypi_uploader.py
@@ -13,7 +13,7 @@ from time import sleep
 from re import findall
 
 # Constants
-ENV_NAME = "pypi_account_info.env"  # CHANGE THIS TO YOUR ENVIRONMENT NAME (.env file)
+ENV_NAME = ".env"  # CHANGE THIS TO YOUR ENVIRONMENT NAME (.env file)
 ENV_PATH = join(dirname(__file__), ENV_NAME)
 
 
